101be14695c1f50bf1792f2949307f7dbb124d96
Alvaro Cabrera <pateketrueke@gmail.com>
2018-03-13 21:02:22 -0600

    Load models from .json files

---
+++ src/models/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const JSONSchemaSequelizer = require('json-schema-sequelizer');
+
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
+const config = require('../../config.js');
+
+const refs = JSONSchemaSequelizer.refs(__dirname);
+
+const db = new JSONSchemaSequelizer(config.use_env_variable
+  ? process.env[config.use_env_variable]
+  : config, refs);
+
+db.connect()
+  .catch(e => {
+    console.error(e.stack);
+    process.exit(1);
+  });
+
+fs
+  .readdirSync(path.join(__dirname, 'schemas'))
+  .filter(file => file.indexOf('.json') !== -1)
+  .forEach(file => {
+    const name = path.basename(file, '.json');
+    const $schema = require(path.join(__dirname, 'schemas', name));
+    const functions = path.join(__dirname, '/../api/models', `${name}.js`);
+    const definition = fs.existsSync(functions)
+      ? require(functions)
+      : {};
+
+    const model = _.merge({ $schema }, definition);
+
+    if (!model.$schema.id) {
+      model.$schema.id = name;
+    }
+
+    Object.defineProperty(db, model.$schema.id, {
+      get() {
+        return db.models[model.$schema.id];
+      },
+    });
+
+    db.add(model);
+  });
+
+module.exports = db;
