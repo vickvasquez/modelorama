7c75324310578a2925219acad4be85b48320d794
Alvaro Cabrera <pateketrueke@gmail.com>
2018-03-14 00:20:50 -0600

    Empty schemas

---
+++ db/schema.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+'use strict';
+module.exports = {
+  up: (queryInterface, dataTypes) => [
+  ],
+  down: (queryInterface, dataTypes) => [
+  ],
+  change: (queryInterface, dataTypes) => [
+  ],
+};
