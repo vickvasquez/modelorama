2dde1a46c8c7dfa930c827ca2e604e8420d9b0e6
Alvaro Cabrera <pateketrueke@gmail.com>
2018-04-03 14:29:25 -0500

    Quick setup for RESTful-api

--- app.js
+++ app.js
@@ -5,120 +5,69 @@ const JSONSchemaSequelizer = require('json-schema-sequelizer');
 const db = require('./src/models');
 const app = require('express')();
 
-function _buildAttachments(Model, baseDir, uploadDir) {
-  const _attachments = [];
-
-  if (Model.options.$schema.properties) {
-    Object.keys(Model.options.$schema.properties).forEach(key => {
-      const x = Model.options.$schema.properties[key].attachment;
-
-      if (x) {
-        const dest = uploadDir || 'uploads';
-
-        _attachments.push({
-          key,
-          dest,
-          baseDir,
-        });
-      }
-    });
-  }
-
-  return _attachments;
-}
-
 db.connect()
   .then(() => {
-    const modelsNames = Object.keys(db.models);
+    app.use(require('body-parser').json());
 
-    app.get('/', (req, res) => {
-      res.json({
-        status: 'ok',
-        response: {
-          models: modelsNames,
+    const dbHook = require('jsonschema-form-mw');
+
+    app.use(require('serve-static')(dbHook.publicDir));
+
+    app.all('/db/:model?/:action?/:id?', (req, res) => {
+      const isJSON = /application\/.*json/.test(req.headers.accept);
+
+      const Model = db.models[req.params.model];
+
+      const opts = {
+        url(modelName, action) {
+          const pk = db.models[modelName].primaryKeyAttribute;
+
+          return !action
+            ? `/db/${modelName}`
+            : `/db/${modelName}/${action === 'new' ? action : `${action}/:${pk}`}`;
         },
-      });
-    });
-
-    app.get('/db/:model/:action?/:id?', (req, res) => {
-      const bodyParams = req.body || {};
-      const modelName = req.params.model;
-      const Model = db.models[modelName];
-
-      const config = {
-        attachments: _buildAttachments(Model, __dirname),
-        payload: bodyParams.payload,
-        where: bodyParams.where,
+        method: req.body._method || req.method,
+        resource: Model ? JSONSchemaSequelizer.resource(db.$refs, Model, {
+          [Model ? Model.primaryKeyAttribute : undefined]: req.params.id || undefined,
+          attachments: Model ? dbHook.buildAttachments(Model, __dirname, 'tmp') : [],
+          payload: req.body.payload,
+          where: req.body.where,
+        }) : undefined,
+        action: req.params.action,
+        fieldId: req.params.id,
+        modelName: req.params.model,
+        modelNames: Object.keys(db.models),
+        modelInstance: Model,
       };
 
-      if (Model.primaryKeyAttribute) {
-        config[Model.primaryKeyAttribute] = req.params.id || null;
-      }
+      dbHook(opts, isJSON)
+        .then(data => {
+          if (!isJSON) {
+            if (!opts.resource) {
+              res.send(`<ul><li>${data.map(x => `<a href="/db/${x}">${x}</a>`).join('</li><li>')}</li></ul>`);
+              return;
+            }
 
-      const resource = JSONSchemaSequelizer.resource(db.$refs, Model, config);
-      const action = req.params.action || 'index';
+            res.send([
+              `<html><head><link rel="stylesheet" href="/styles.css"/></head>`,
+              `<body><script type="application/json" data-component="jsonschema-form">${JSON.stringify(data)}</script>`,
+              `<script src="/main.js"></script></body></html>`,
+            ].join(''));
+            return;
+          }
 
-      resource.options.isNew = action === 'new';
-      resource.options.action = action;
-      resource.options.actions = {};
-      resource.options.actions[Model.name] = {
-        index: {
-          verb: 'GET',
-          path: '/',
-        },
-      };
-
-      ['new', 'create', 'edit', 'show', 'update', 'destroy'].forEach(method => {
-        resource.options.actions[Model.name][method] = {
-          verb: 'GET',
-          path: '/',
-        };
-      });
-
-      if (!/application\/.*json/.test(req.headers.accept)) {
-        const prefixURL = '//rawgit.com/pateketrueke/0a6693268093ceecd973aae49e55939e/raw/2d9d98b29a2e7569e1744be09c31d95c37b3f44b';
-
-        res.send(`
-          <html>
-            <head>
-              <link rel="stylesheet" href="${prefixURL}/application.css"/>
-            </head>
-            <body>
-              <script type="application/json" data-component="jsonschema-form">${JSON.stringify(resource.options)}</script>
-              <script src="${prefixURL}/application.js"></script>
-            </body>
-          </html>
-        `);
-        return;
-      }
-
-      switch (action) {
-        case 'new':
-          res.json(resource.options);
-          break;
-
-        case 'index':
-          resource.actions.findAll()
-            .then(result => {
-              res.json(result);
+          res.json(data);
+        })
+        .catch(e => {
+          if (isJSON) {
+            res.json({
+              result: e,
             });
-          break;
+            return;
+          }
 
-        case 'edit':
-        case 'show':
-          resource.actions.findOne()
-            .then(result => {
-              res.json(result);
-            });
-          break;
-
-        default:
-          res.json({
-            status: 'err',
-            response: 'Not found',
-          });
-          break;
-      }
+          res.send(e.toString());
+        });
     });
 
     app.listen(3001, () => {
