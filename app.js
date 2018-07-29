8bf9500e4264692ee2ca98444f1b031947f9ba7b
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-28 19:52:55 -0500

    Integrate apollo

--- app.js
+++ app.js
@@ -1,19 +1,78 @@
 'use strict';
 
 const db = require('./src/schema/models');
+
+const JST = require('json-schema-to');
 const app = require('express')();
 
-db.connect()
-  .then(() => {
-    app.use(require('body-parser').json());
-    app.use(require('jsonschema-form-mw')(db));
+async function main() {
+  await db.connect();
 
-    const port = process.env.PORT || 8081;
+  const { ApolloServer } = require('apollo-server-express');
 
-    app.listen(port, () => {
-      console.log(`Listening at http://localhost:${port}/db`);
-    });
-  })
+  const _schemas = db.schemas;
+  const _bundle = await JST.load(_schemas);
+  const _jst = JST.merge('webapp', _bundle);
+
+  const modelSchema = _jst.graphql;
+  const baseSchema = `
+    type Query {
+      dummy: [String]
+    }
+    type Mutation {
+      dummy: [String]
+    }
+    schema {
+      query: Query
+      mutation: Mutation
+    }
+  `;
+
+  const jsf = require('json-schema-faker');
+
+  const {
+    Cart, CartList, Product, ProductList,
+  } = _jst.$refs;
+
+  const resolvers = {
+    Mutation: {},
+    Query: {
+      Cart() {
+        return jsf(Cart, _schemas);
+      },
+    },
+  };
+
+  const typeDefs = [
+    baseSchema,
+    modelSchema,
+  ];
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: req => req,
+    formatError: error => _.omit(error, ['locations']),
+  });
+
+  server.applyMiddleware({
+    app,
+    path: '/api',
+  });
+
+  app.use(require('body-parser').json());
+  app.use(require('jsonschema-form-mw')(db));
+
+  const port = process.env.PORT || 8081;
+
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/db`);
+    console.log(`GraphQL: http://localhost:${port}/api`);
+  });
+}
+
+Promise.resolve()
+  .then(() => main())
   .catch(error => {
     console.log(error);
     process.exit(1);
